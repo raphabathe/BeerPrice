6454503ed47b87541c56152ad6f3f1fd4d191637
ClickDois <ClickDois@iMac-de-Rapha.local>
2018-10-05 15:18:04 -0300

    versao1

--- BeerPrice/app/src/main/java/com/digitalhouse/beerprice/BeerPriceActivity.java
+++ BeerPrice/app/src/main/java/com/digitalhouse/beerprice/BeerPriceActivity.java
@@ -1,9 +1,13 @@
 package com.digitalhouse.beerprice;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,11 +23,14 @@ public class BeerPriceActivity extends AppCompatActivity {
     private TextView ml2;
     private TextView melhorCusto;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_price);
-
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.beer_icon_bar);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         valor1 = findViewById(R.id.valor_cerveja_1);
         valor2 = findViewById(R.id.valor_cerveja_2);
         ml1= findViewById(R.id.ml_cerveja_1);
@@ -34,14 +41,9 @@ public class BeerPriceActivity extends AppCompatActivity {
 
 
 
-
-
     public void calcular(View view){
        float preco100Ml1 = ((Float.parseFloat(valor1.getText().toString())/Float.parseFloat(ml1.getText().toString())*100));
        float preco100Ml2 = ((Float.parseFloat(valor2.getText().toString())/Float.parseFloat(ml2.getText().toString()))*100);
-
-
-
        if (preco100Ml1 > preco100Ml2){
            melhorCusto.setText("Cerveja 2") ;
        }else if (preco100Ml2 > preco100Ml1){
@@ -49,10 +51,6 @@ public class BeerPriceActivity extends AppCompatActivity {
        }else {
            melhorCusto.setText("Tanto faz") ;
        }
-
-
-
     }
-
 
 }
